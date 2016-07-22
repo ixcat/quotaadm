9f4fe4041b298fcf2d3f7d4e152f5419fdedda2d
Chris Turner <cat@199tech.net>
2016-07-22 11:43:31 -0500

    quotaadm: add ConvertQuota.pm to manifest and revbump for bugfix/new features

--- lib/Quota/Adm.pm
+++ lib/Quota/Adm.pm
@@ -4,6 +4,6 @@ package Quota::Adm;
 # this package only used by ExtUtils at present - see docs in source distrib
 # for more details concerning usage and software architecture.
 
-$VERSION = 0.10;
+$VERSION = 0.11;
 
 1;
